29415282a192898bf16de69a49ae673b02072e0e
Florent Urbain <florenturbain@protonmail.com>
2025-03-16 16:54:35 +0100

    fix: CartModal UI

--- src/components/Cart/CartModal.jsx
+++ src/components/Cart/CartModal.jsx
@@ -1,45 +1,46 @@
-import { Button, Modal } from 'react-bootstrap';
-import testList from '../Product/testList';
+import { Button, Modal } from "react-bootstrap";
+import testList from "../Product/testList";
 
 function CartModal({ open, onClose }) {
+  
+  const totalAmount = testList.reduce((sum, product) => sum + product.price * product.quantity, 0);
+
   return (
-    <>
-      <Modal show={open} onHide={onClose}>
-        <Modal.Header closeButton>
-          <Modal.Title>Cart</Modal.Title>
-        </Modal.Header>
-        <Modal.Body>
-          <table>
-            <thead>
-                <tr>
-                  <th scope="col"> Ref </th>
-                  <th scope="col"> Name </th>
-                  <th scope="col"> Price (€)</th>
-                  <th scope="col"> Amount </th>
-                </tr>
-              </thead>
-              <tbody>
-                {testList.map((product) => (
-                  <tr key={product._id}>
-                    <td scope='row'>{product.brand}</td>
-                    <td>{product.name}</td>
-                    <td>{product.price}</td>
-                    <td>{product.quantity}</td>
-                  </tr>
-                ))}
-              </tbody>
-          </table>
-        </Modal.Body>
-        <Modal.Footer>
-          <Button variant="secondary" onClick={onClose}>
-            Close
-          </Button>
-            <Button variant="primary" onClick={onClose}>
-            Order
-          </Button>
-        </Modal.Footer>
-      </Modal>
-    </>
-  )}
+    <Modal show={open} onHide={onClose} centered>
+      <Modal.Header closeButton>
+        <Modal.Title>Cart</Modal.Title>
+      </Modal.Header>
+      <Modal.Body>
+        <table className="table table-striped table-bordered text-center">
+          <thead className="table-light">
+            <tr>
+              <th scope="col">Ref</th>
+              <th scope="col">Name</th>
+              <th scope="col">Price (€)</th>
+              <th scope="col">Amount</th>
+            </tr>
+          </thead>
+          <tbody>
+            {testList.map((product) => (
+              <tr key={product._id}>
+                <td>{product._id}</td>
+                <td>{product.name}</td>
+                <td>{product.price}</td>
+                <td>{product.quantity}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+        <h4 className="text-end fw-bold">Total Amount: {totalAmount}€</h4>
+      </Modal.Body>
+      <Modal.Footer>
+        <Button variant="secondary" onClick={onClose}>
+          Close
+        </Button>
+        <Button variant="primary">Order</Button>
+      </Modal.Footer>
+    </Modal>
+  );
+}
 
 export default CartModal;
